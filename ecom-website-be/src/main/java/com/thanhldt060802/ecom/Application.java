90c0d6683a4cf41f3817b13e740c90c19ee0029f
Le Dao Tan Thanh <thanhldt060802@gmail.com>
2025-04-15 16:14:23 +0700

    init project

---
+++ ecom-website-be/src/main/java/com/thanhldt060802/ecom/Application.java
@@ -0,0 +1,13 @@
+package com.thanhldt060802.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
